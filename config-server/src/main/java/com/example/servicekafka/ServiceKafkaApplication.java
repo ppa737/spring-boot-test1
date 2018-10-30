20b0d8f15b1fc7c59735dc04666a2d35f5a7170c
ppa737 <ppa747@163.com>
2018-10-30 16:50:03 +0800

    save

---
+++ config-server/src/main/java/com/example/servicekafka/ServiceKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.example.servicekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceKafkaApplication.class, args);
+    }
+}
